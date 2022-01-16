229da5ad2ce4559c6397281dd3865c0d2344bf2c
Matěj Klimeš <klimes.matej1@gmail.com>
2022-01-16 18:06:22 +0100

    1.0.1 Prepared classes and functions for upgrade

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 let draw_width = "3";
 let is_drawing = false;
 
+let dragging = false;
 let save_image_data;
 let stroke_color = "black";
 let fill_color = "black"
@@ -141,3 +142,86 @@ class PolygonPoint {
 let shape_bounding_box = new ShapeBoundingBox(0, 0, 0, 0);
 let mouse_down = new MouseDownPos(0, 0);
 let loc = new Location(0, 0);
+
+function get_mouse_position(x, y) {
+    let canvasSizeData = canvas.getBoundingClientRect();
+    return { x: (x - canvasSizeData.left) * (canvas.width / canvasSizeData.width), y: (y - canvasSizeData.top) * (canvas.height / canvasSizeData.height) };
+}
+
+function save_canvas_image() {
+    save_image_data = context.getImageData(0,0, canvas.width, canvas.height);
+}
+
+function redraw_canvas_image() {
+    context.putImageData(save_image_data,0,0);
+}
+
+function update_rubberband_size_data(loc) {
+    shape_bounding_box.width = Math.abs(loc.x - mouse_down.x);
+    shape_bounding_box.height = Math.abs(loc.y - mouse_down.y);
+
+    if (loc.x > mouse_down.x) {
+        shape_bounding_box.left = mouse_down.x;
+    } else {
+        shape_bounding_box.left = loc.x;
+    }
+
+    if (loc.y > mouse_down.y) {
+        shape_bounding_box.top = mouse_down.y;
+    } else {
+        shape_bounding_box.top = loc.y;
+    }
+}
+
+function get_angle(mouselocX, mouselocY) {
+    let adjecent = mouse_down.x - mouselocX;
+    let opposite = mouse_down.y - mouselocY;
+
+    return radians_to_degrees(Math.atan2(opposite, adjecent));
+}
+
+function radians_to_degrees(rad) {
+    return (rad * (180 / Math.PI)).toFixed(2);
+}
+
+function degrees_to_radians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+function react_to_mouse_down(e) {
+    canvas.style.cursor = "crosshair";
+    loc = get_mouse_position(e.clientX, e.clientY);
+    save_canvas_image();
+    mouse_down.x = loc.x;
+    mouse_down.y = loc.y;
+    dragging = true;
+}
+
+function react_to_mouse_move(e) {
+    canvas.style.cursor = "crosshair";
+    loc = get_mouse_position(e.clientX, e.clientY);
+}
+
+function react_to_mouse_move(e) {
+    canvas.style.cursor = "default";
+    loc = get_mouse_position(e.clientX, e.clientY);
+    redraw_canvas_image();
+    update_rubberband_on_move(loc);
+    dragging = false;
+    usingBrush = false;
+}
+
+function save_image() {
+    var img_file = document.getElementById("save");
+    img_file.setAttribute("download", "paint.jpg");
+    img_file.setAttribute("href", canvas.toDataURL());
+}
+
+function open_image() {
+    let img = new Image();
+    img.onload = function() {
+        context.clearRect(0,0, canvas.width, canvas.height);
+        context.drawImage(img,0,0);
+    }
+    img.src = "paint.png";
+}
